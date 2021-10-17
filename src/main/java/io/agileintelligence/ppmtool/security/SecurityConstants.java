ddf51fa4d458f976e2dd33c80fc09af42b75b89a
mohd5289 <mohd5289@yahoo.com>
2021-10-17 23:58:05 +0100

    Changed Token

--- src/main/java/io/agileintelligence/ppmtool/security/SecurityConstants.java
+++ src/main/java/io/agileintelligence/ppmtool/security/SecurityConstants.java
@@ -10,6 +10,6 @@ public class SecurityConstants {
 
 	public static final String TOKEN_PREFIX = "Bearer ";
 	
-	public static final long EXPIRATION_TIME = 300_000;
+	public static final long EXPIRATION_TIME = 3000000;
 	 public static final String HEADER_STRING= "Authorization";
 }
